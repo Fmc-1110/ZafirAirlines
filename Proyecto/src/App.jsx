da7b6a0e3ac80cf80c080988ea1ba304ff167299
Fmc-1110 <fmcerdas1114@gmail.com>
2024-04-04 22:24:27 -0600

    app

--- Proyecto/src/App.jsx
+++ Proyecto/src/App.jsx
@@ -1,12 +1,15 @@
 import Registro from './Components/Registro'
 import Inicio from './Components/Inicio'
 import IniciarSesión from './Components/IniciarSesion'
+import Roles from './Components/Roles'
+import CrearUsuario from './Components/CrearUsuario'
+import CambiarContraseña from './Components/CambiarContraseña'
 function App() {
 
   return (
   <div>
 
-    <Inicio></Inicio>  
+    <CambiarContraseña></CambiarContraseña>  
   </div>
 
   )
